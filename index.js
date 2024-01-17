cb3226c70bf5dc0df8f755c9f32d24983af03a5e
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-01-17 09:12:56 +0530

    catch changed

--- index.js
+++ index.js
@@ -16,12 +16,13 @@ mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
     console.log("connected to MONGODB...");
+  })
+  .catch((error) => {
+    console.log("error connecting to MONGODB:", error.message);
+  });
 
     // start the server
     app.listen(config.PORT, () => {
       console.log(`Server running on port ${config.PORT}`);
     });
-  })
-  .catch((error) => {
-    console.log("error connecting to MONGODB:", error.message);
-  });
+ 
