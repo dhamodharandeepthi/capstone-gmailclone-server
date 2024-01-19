0360507bfba9a6cc77c1980f1aec526535e08852
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-01-19 11:06:20 +0530

    dist added

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const mongoose = require("mongoose");
 const config = require("./utils/config");
 const routes = require("./routes/route");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 // app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", routes);
+
+app.use(express.static(path.join(__dirname, "./client/dist")));
+
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/dist/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
 
 // Mongodb connection
 console.log("connecting to mongodb...");
@@ -21,8 +33,7 @@ mongoose
     console.log("error connecting to MONGODB:", error.message);
   });
 
-    // start the server
-    app.listen(config.PORT, () => {
-      console.log(`Server running on port ${config.PORT}`);
-    });
- 
+// start the server
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
+});
