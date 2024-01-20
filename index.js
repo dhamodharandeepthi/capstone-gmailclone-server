bca4f3d1e5b894fe39a88bba650c69375f122053
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-01-20 12:02:35 +0530

    welcome added

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ app.use(cors());
 // app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+app.get("*", (req, res) => {
+  res.status(200).json({ message: "welcome to gmailclone app" })
+});
 
 app.use("/", routes);
 
